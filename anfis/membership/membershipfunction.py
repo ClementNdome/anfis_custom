44093415d5ce57916efea43607fd017be1ce7614
ClementNdome <clementndome20@gmail.com>
2025-09-25 21:33:40 +0300

    modified files to meet crop yield ANFIS xtics

--- anfis/membership/membershipfunction.py
+++ anfis/membership/membershipfunction.py
@@ -1,16 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar 31 15:41:58 2014
-
-@author: tim.meggs
-"""
-
+import numpy as np
 from skfuzzy import gaussmf, gbellmf, sigmf
 
 class MemFuncs:
-    'Common base class for all employees'
     funcDict = {'gaussmf': gaussmf, 'gbellmf': gbellmf, 'sigmf': sigmf}
-
 
     def __init__(self, MFList):
         self.MFList = MFList
@@ -18,21 +10,16 @@ class MemFuncs:
     def evaluateMF(self, rowInput):
         if len(rowInput) != len(self.MFList):
             print("Number of variables does not match number of rule sets")
+        return [[self.funcDict[self.MFList[i][k][0]](rowInput[i], **self.MFList[i][k][1]) 
+                 for k in range(len(self.MFList[i]))] for i in range(len(rowInput))]
 
-        return [[self.funcDict[self.MFList[i][k][0]](rowInput[i],**self.MFList[i][k][1]) for k in range(len(self.MFList[i]))] for i in range(len(rowInput))]
-    
-
-    import torch
-
-class GaussMF(torch.nn.Module):
+class GaussMF:
     def __init__(self, mean: float, std: float):
-        super().__init__()
-        self.mean = torch.nn.Parameter(torch.tensor(float(mean)))
-        self.std = torch.nn.Parameter(torch.tensor(float(std)))
+        self.mean = float(mean)
+        self.std = float(std)
 
-    def forward(self, x):
-        return torch.exp(-((x - self.mean) ** 2) / (2 * (self.std ** 2)))
-
+    def __call__(self, x):
+        return np.exp(-((x - self.mean) ** 2) / (2 * (self.std ** 2)))
 
 def make_gauss_mfs(num_inputs=None, num_mfs=None, centers=None, sigmas=None, mu=None, sigma=None):
     """
@@ -42,10 +29,8 @@ def make_gauss_mfs(num_inputs=None, num_mfs=None, centers=None, sigmas=None, mu=
       - old style (mu, sigma single values)
     """
     if mu is not None and sigma is not None:
-        # old-style single Gaussian MF
         return GaussMF(mean=mu, std=sigma)
 
-    # new-style: full MF grid
     mfs = []
     for i in range(num_inputs):
         mfs_for_input = []
@@ -54,4 +39,3 @@ def make_gauss_mfs(num_inputs=None, num_mfs=None, centers=None, sigmas=None, mu=
             mfs_for_input.append(mf)
         mfs.append(mfs_for_input)
     return mfs
-
